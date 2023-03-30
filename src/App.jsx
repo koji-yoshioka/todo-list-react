330591ff867ba17171101411f1582dec64ba93d3
github koji-yoshioka <koji.y.japan@gmail.com>
2023-03-31 02:00:34 +0900

    refactor: common header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
-import { Header } from "./components/organisms/Header";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,12 +1,13 @@
-import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
-import { Route } from "react-router-dom";
+import logo from "./assets/reactjs-icon.svg";
+import styles from "./styles.module.css";
 
 export const Header = ({ props }) => {
   return (
     <header className={styles.module}>
-      <Link to="/">
-        <h1>plain-react</h1>
+      <Link to="/" className={styles.logoArea}>
+        <img src={logo} alt="" className={styles.logoArea__logo} />
+        <h1 className={styles.logoArea__title}>Plain-React</h1>
       </Link>
     </header>
   );
